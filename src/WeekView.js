82394dbe0193180f769a690fd73512bc771bc2bd
paupalou <paupalou@gmail.com>
2019-01-29 16:23:57 +0100

    improving..

--- src/WeekView.js
+++ src/WeekView.js
@@ -1,303 +1,137 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import {
   isToday,
   isSameDay,
-  addWeeks,
-  subWeeks,
   isSameMonth,
   addDays,
   subDays,
-  isSameWeek
 } from 'date-fns';
 import { getWeekOfDay } from './dates';
 import classNames from 'classnames';
 
 import './WeekView.scss';
 
-const TOP = 'TOP';
-const BOTTOM = 'BOTTOM';
-
 const Week = props => {
   const today = new Date();
 
-  const [viewDate, setViewDate] = useState(today);
-  // const [touchStartY, setTouchStartY] = useState();
-  // const [lastTouchY, setLastTouchY] = useState();
+  const [days, setDays] = useState(getWeekOfDay(today));
   const [scrolling, setScrolling] = useState(false);
+  const [scrollInterval, setScrollInterval] = useState();
 
-  const prevWeek = getWeekOfDay(subWeeks(viewDate, 1));
-  const currentWeek = getWeekOfDay(viewDate);
-  const nextWeek = getWeekOfDay(addWeeks(viewDate, 1));
-
-  const [touchesVAxis, setTouchesVAxis] = useState([]);
+  const [origin, setOrigin] = useState();
 
   const [scrollingDays, setScrollingDays] = useState(
-    getWeekOfDay(viewDate)
+    getWeekOfDay(today)
   );
 
-  // const [scrollingDirections, setScrollingDirections] = useState([]);
+  const [scrollingFirstDay, setScrollingFirstDay] = useState(
+    getWeekOfDay(today)[0]
+  );
 
-  // const scroll = (days, direction) => {
-  //   switch (direction) {
-  //     case TOP:
-  //       setScrollingDays([
-  //         subDays(days[0], 1),
-  //         ...days.slice(0, days.length - 1)
-  //       ]);
-  //       break;
-  //     case BOTTOM:
-  //       setScrollingDays([
-  //         ...days.slice(1, days.length),
-  //         addDays(days.slice(-1).pop(), 1)
-  //       ]);
-  //       break;
-  //     default:
-  //       break;
-  //   }
-  // };
-
-  // const daysColumnEl = useRef();
+  const daysColumn = useRef();
   const arrows = useRef();
 
-  const wheelMove = (event) => {
-    event.deltaY < 0
-      ? setViewDate(subWeeks(viewDate, 1))
-      : setViewDate(addWeeks(viewDate, 1));
-  }
+  // const wheelMove = (event) => {
+  //   event.deltaY < 0
+  //     ? setViewDate(subWeeks(viewDate, 1))
+  //     : setViewDate(addWeeks(viewDate, 1));
+  // }
+
+  useEffect(() => {
+    daysColumn.current.addEventListener('touchmove', touchMove);
+
+    return () => {
+      console.log('unmounting');
+      daysColumn.current.removeEventListener('touchmove', touchMove);
+    }
+  }, [origin, scrollingFirstDay, scrollInterval]);
 
   const touchEnd = (event) => {
     console.log('TOUCH END');
-    // if (!touchStartY) { return; }
-
-    // setTouchStartY(undefined);
-    // setLastTouchY(undefined);
-
-    // setScrollingDays([...currentWeek]);
-    // setScrollingDirections([]);
-    // console.log('touchesVAxis: ', touchesVAxis)
-    // setTouchesVAxis([]);
-    // setScrollingDays(getWeekOfDay(viewDate));
     setScrolling(false);
-    return;
+    setDays(getWeekOfDay(scrollingFirstDay));
+    setOrigin(undefined);
+    if (scrollInterval) {
+      clearInterval(scrollInterval);
+      setScrollInterval(undefined);
+    }
+  }
 
-    // const touchEndY = event.changedTouches[0].clientY;
-    // if (touchStartY > touchEndY + 40) {
-    //   setViewDate(addWeeks(viewDate, 1));
-    // } else if (touchStartY < touchEndY - 40) {
-    //   setViewDate(subWeeks(viewDate, 1));
-    // }
-
-    // setTouchStartY(undefined);
-    // setDayDirection(undefined);
+  const touchStart = (event) => {
+    setOrigin(event.changedTouches[0].clientY);
   }
 
   const touchMove = (event) => {
-    event.preventDefault();
-    console.log('touchMove event');
-    setScrolling(true);
-
+    console.log('touchMove');
     const y = event.changedTouches[0].clientY;
+    console.log(`touchMove event y ${y} origin ${origin}`)
+
+    event.preventDefault();
+    setScrolling(true);
 
     arrows.current.style.top = y - 40 + 'px';
     arrows.current.style.left = '10vw';
 
-    // console.log('touchMove touchesVAxis: ', touchesVAxis);
+    // if (origin < y) {
+    // console.log('scrolling top to previousDay = ', previousDay)
+    // console.log('scrollingDays', scrollingDays)
 
-    if (touchesVAxis.length < 10) {
-      setTouchesVAxis([...touchesVAxis, y]);
+    let interval;
+    if (!scrollInterval) {
+      interval = setInterval(() => {
+        const previousDay = subDays(scrollingFirstDay, 1);
+
+        console.log('fire interval func', Math.random())
+        // setScrollingDays([
+        //   previousDay,
+        //   ...scrollingDays.slice(0, scrollingDays.length - 1)
+        // ]);
+        setScrollingFirstDay(previousDay, 1);
+      }, 1000);
+
+      setScrollInterval(interval);
     }
 
-    return;
 
-    // console.log(`finished saving 10 touchesVAxis`);
-    // // compare
-    // if (touchesVAxis[0] > touchesVAxis[touchesVAxis.length -1]) {
-    //   console.log('SCROLL TOP')
-    //   const previousDay = subDays(scrollingDays[0], 1);
-    //   const prevDayIndex = prevWeek.findIndex(
-    //     day => isSameDay(day, previousDay)
-    //   );
 
-    //   if (prevDayIndex) {
-    //     console.log(`day found on index ${prevDayIndex}`);
-    //   } else {
-    //     console.log('full week LOADED !!!!!!!!');
-    //     touchEnd(event);
-    //     return;
-    //   }
-
-    //   // setScrollingDays([
-    //   //   prevWeek[prevDayIndex],
-    //   //   ...scrollingDays.slice(0, scrollingDays.length - 1),
-    //   // ]);
     // } else {
-    //   console.log('SCROLL BOTTOM')
+      // const nextDay = addDays(scrollingDays.slice(-1).pop(), 1);
+
+      // setInterval(() => {
+      //   setScrollingDays([
+      //     ...scrollingDays.slice(1, scrollingDays.length),
+      //     nextDay
+      //   ]);
+      // }, 500)
     // }
 
-    // setTouchesVAxis([]);
+    setOrigin(y);
   }
 
-    // if (!touchStartY) {
-    //   setTouchStartY(y);
-    // }
-    // setScrolling(true);
-
-    // setLastTouchY(y);
-    // console.log(lastTouchY);
-    // return;
-
-    // if (y < lastTouchY) {
-      // console.log('TOP');
-      // setScrollingDirections([...scrollingDirections, TOP]);
-      // scroll(scrollingDays, TOP);
-    // } else {
-    //   console.log('BOTTOM');
-    //   // setScrollingDirections([...scrollingDirections, BOTTOM]);
-    //   scroll(scrollingDays, BOTTOM);
-    // }
-
-    // setLastTouchY(y);
-
-    // arrows.current.style.top = y - 40 + 'px';
-    // arrows.current.style.left = '12vw';
-
-    // return;
-
-    // console.log('scrollingDirections: ', scrollingDirections);
-
-    // const scrollDirectionsLength = scrollingDirections.length;
-    // // console.log(scrollingDirections);
-    // if (scrollDirectionsLength >= 5 && scrollDirectionsLength % 5 === 0) {
-    //   const lastScrollDirections = scrollingDirections.slice(
-    //     scrollDirectionsLength - 5
-    //   );
-    //   const movingInDirection = lastScrollDirections.every((item, i) => {
-    //     if (i === 0) {
-    //       return true;
-    //     }
-    //     return item === lastScrollDirections[i-1];
-    //   });
-
-
-    //   if (movingInDirection) {
-    //     const dayDirection = scrollingDirections.slice(-1).pop();
-    //     console.log('LAST 5 EQUAL', dayDirection);
-
-    //     switch (dayDirection) {
-    //       case TOP:
-    //         scroll(scrollingDays, TOP);
-    //         break;
-    //       case BOTTOM:
-    //         scroll(scrollingDays, BOTTOM);
-    //         break;
-    //       default:
-    //         break;
-    //     }
-    //   }
-    // }
-
-  // }
-
-
-  // useEffect(() => {
-  //   console.log('attaching events');
-  //   if ('ontouchstart' in window) {
-  //     daysColumnEl.current.addEventListener('touchmove', touchMove);
-  //   } else if ('onwheel' in window) {
-  //     daysColumnEl.current.addEventListener('wheel', wheelMove);
-  //   }
-
-  //   return () => {
-  //     console.log('unmount');
-  //     daysColumnEl.current.removeEventListener('touchmove', touchMove);
-  //     daysColumnEl.current.removeEventListener('wheel', wheelMove);
-  //   }
-  // },[touchStartY, lastTouchY, scrollingDays]);
-
-  // useEffect(() => {
-  //   console.log('attaching events2');
-  //   if ('ontouchstart' in window) {
-  //     document.addEventListener('touchend', touchEnd);
-  //   }
-
-  //   return () => {
-  //     console.log('unmount2');
-  //     document.removeEventListener('touchend', touchEnd);
-  //   }
-  // },[touchStartY]);
-
-
-
-
-  const dayColumnClass = classNames({
+  const daysColumnClass = classNames({
     days: true,
     scrolling
   });
-
-  // const days = scrolling ? scrollingDays : currentWeek;
-
-  const checkDirection = directions => {
-    return directions[0] > directions[9] ? TOP : BOTTOM;
-  }
-
-  // const days = currentWeek;
-
-    //   const previousDay = subDays(scrollingDays[0], 1);
-    //   const prevDayIndex = prevWeek.findIndex(
-    //     day => isSameDay(day, previousDay)
-    //   );
-
-  if (touchesVAxis.length === 10) {
-    console.log('NOEW');
-    const scrollDirection = checkDirection(touchesVAxis);
-    switch (scrollDirection) {
-      case TOP:
-        const previousDay = subDays(scrollingDays[0], 1);
-        const prevDayIndex = prevWeek.findIndex(
-          day => isSameDay(day, previousDay)
-        );
-
-        if (prevDayIndex > -1) {
-          setScrollingDays(
-            [
-              prevWeek[prevDayIndex],
-              ...scrollingDays.slice(0, scrollingDays.length - 1)]
-          );
-          setTouchesVAxis([]);
-        }
-
-        console.log(scrollingDays);
-        break;
-      default:
-    }
-  }
-
-    // if (touchesVAxis[0] > touchesVAxis[touchesVAxis.length -1]) {
-    //   console.log('SCROLL TOP')
-    //   const previousDay = subDays(scrollingDays[0], 1);
-    //   const prevDayIndex = prevWeek.findIndex(
-    //     day => isSameDay(day, previousDay)
-    //   );
-  //
-  //
-  //
-  //
-  const days = scrolling ?
-    []
-    : [...prevWeek, ...currentWeek, ...nextWeek];
 
   const scrollbarClass = classNames({
     scrolling
   });
 
+  const sDays = [scrollingFirstDay];
+
+  Array(6).fill(0,0).forEach((_, i) => sDays[i+1] = addDays(sDays[i], 1));
+
+  console.log('sdays', sDays);
+
   return (
     <div id="week-view">
       <div
-        className={dayColumnClass}
-        onTouchMove={touchMove}
+        ref={daysColumn}
+        className={daysColumnClass}
+        onTouchStart={touchStart}
         onTouchEnd={touchEnd}
       >
-        { currentWeek.map(day => (
+        { days.map(day => (
           <Day key={day.getTime()}
             day={day}
             select={props.setSelectedDay}
@@ -309,7 +143,7 @@ const Week = props => {
 
       <div id="week-scrollbar" className={scrollbarClass}>
         {
-          scrollingDays.map(day => (
+          sDays.map(day => (
           <Day key={day.getTime()}
             day={day}
             select={props.setSelectedDay}
@@ -318,7 +152,7 @@ const Week = props => {
         )}
       </div>
 
-      <Content week={currentWeek} />
+      <Content week={sDays} />
 
     </div>
   )
