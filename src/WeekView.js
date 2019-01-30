bd167a95d1ff8a64c990fd9d3b9b58d8f3a7503a
paupalou <paupalou@gmail.com>
2019-01-30 16:26:24 +0100

    simplified week scrool

--- src/WeekView.js
+++ src/WeekView.js
@@ -1,159 +1,80 @@
-import React, { useEffect, useState, useRef } from 'react';
+import React, { useState } from 'react';
 import {
   isToday,
   isSameDay,
   isSameMonth,
-  addDays,
-  subDays,
+  addWeeks,
+  subWeeks
 } from 'date-fns';
 import { getWeekOfDay } from './dates';
 import classNames from 'classnames';
+import posed, { PoseGroup } from 'react-pose';
 
 import './WeekView.scss';
-
-import { debounce } from './utils';
 
 const Week = props => {
   const today = new Date();
 
   const [days, setDays] = useState(getWeekOfDay(today));
-  const [scrolling, setScrolling] = useState(false);
-  const [origin, setOrigin] = useState();
-  const [scrollingDays, setScrollingDays] = useState(
-    getWeekOfDay(today)
-  );
 
-  const daysColumn = useRef();
-  const arrows = useRef();
-
-  // const wheelMove = (event) => {
-  //   event.deltaY < 0
-  //     ? setViewDate(subWeeks(viewDate, 1))
-  //     : setViewDate(addWeeks(viewDate, 1));
-  // }
-
-  useEffect(() => {
-    console.log('adding listeners');
-
-    daysColumn.current.addEventListener('touchstart', touchStart);
-    daysColumn.current.addEventListener('touchmove', touchMove);
-    daysColumn.current.addEventListener('touchend', touchEnd);
-
-    return () => {
-      console.log('removing listeners');
-      daysColumn.current.removeEventListener('touchstart', touchStart);
-      daysColumn.current.removeEventListener('touchmove', touchMove);
-      daysColumn.current.removeEventListener('touchend', touchEnd);
-    }
-  }, [origin, scrollingDays]);
-
-
-  const touchEnd = (event) => {
-    event.preventDefault();
-    console.log('TOUCH END');
-    setOrigin(undefined);
-    setScrolling(false);
-    setDays(scrollingDays);
+  const showPrevWeek = (event) => {
+    const prevWeek = getWeekOfDay(subWeeks(days[0], 1));
+    setDays(prevWeek)
   }
 
-  const touchStart = (event) => {
-    event.preventDefault();
-    console.log('touchStart');
-    setOrigin(event.changedTouches[0].clientY);
+  const showNextWeek = (event) => {
+    const nextWeek = getWeekOfDay(addWeeks(days[0], 1));
+    setDays(nextWeek);
   }
 
-  const touchMove = (event) => {
-    event.preventDefault();
-    if (typeof origin === 'undefined') {
-      return;
-    }
-    const y = event.changedTouches[0].clientY;
-    console.log(`touchMove ${origin}`);
-
-    setScrolling(true);
-
-    arrows.current.style.top = y - 40 + 'px';
-    arrows.current.style.left = '10vw';
-
-    const previousDay = subDays(scrollingDays[0], 1);
-    const nextDay = addDays(scrollingDays[scrollingDays.length - 1], 1);
-
-    if (origin > y) {
-      setScrollingDays([
-        previousDay,
-        ...scrollingDays.slice(0, scrollingDays.length - 1)
-      ]);
-    } else {
-      setScrollingDays([
-        ...scrollingDays.slice(1, scrollingDays.length),
-        nextDay
-      ]);
-    }
-
-    setOrigin(y);
-  }
-
-  const daysColumnClass = classNames({
-    days: true,
-    scrolling
-  });
-
-  const scrollbarClass = classNames({
-    scrolling
+  const AnimatedDay = posed.div({
+    enter: { opacity: 1 },
+    exit: { opacity: 0 },
   });
 
   return (
     <div id="week-view">
-      <div
-        ref={daysColumn}
-        className={daysColumnClass}
-      >
-        { days.map(day => (
-          <Day key={day.getTime()}
-            day={day}
-            select={props.setSelectedDay}
-            selectedDay={props.selectedDay}
-          />)
-        )}
-        <i ref={arrows} className="arrows fas fa-arrows-alt-v" />
+      <div className="days">
+        <button onClick={showPrevWeek}><i class="fas fa-chevron-up" /></button>
+        <PoseGroup>
+          {
+            days.map(day => {
+              const selected = isSameDay(props.selectedDay, day);
+              const dayClass = classNames({
+                day: true,
+                today: isToday(day),
+                selected
+              });
+
+              return (
+                <AnimatedDay
+                  className={dayClass}
+                  key={day.getTime()}
+                  onClick={() => props.setSelectedDay(day)}
+                >
+                  <Day day={day} />
+                </AnimatedDay>
+              )
+            })
+          }
+
+        </PoseGroup>
+        <button onClick={showNextWeek}><i class="fas fa-chevron-down" /></button>
       </div>
 
-      <div id="week-scrollbar" className={scrollbarClass}>
-        {
-          scrollingDays.map(day => (
-          <Day key={day.getTime()}
-            day={day}
-            select={props.setSelectedDay}
-            selectedDay={props.selectedDay}
-          />)
-        )}
-      </div>
-
-      <Content week={scrollingDays} />
-
+      <Content week={days} />
     </div>
   )
 };
 
-const Day = ({ day, select, selectedDay, visible }) => {
-  const selected = isSameDay(selectedDay, day);
-  const dayClass = classNames({
-    day: true,
-    today: isToday(day),
-    selected,
-    visible
-  })
-  const selectDay = () => select(day);
-
-  return (
-    <div className={dayClass} onClick={selectDay}>
-      <span className="number">{day.getDate()}</span>
-      <span className="name">
-        {day.toLocaleDateString('en-US', { weekday: 'short' })}
-      </span>
-    </div>
-  );
-};
+const Day = ({ day }) => (
+  <>
+    <span className="number">{day.getDate()}</span>
+    <span className="name">
+      {day.toLocaleDateString('en-US', { weekday: 'short' })}
+    </span>
+  </>
+);
 
 const Content = props => {
   const len = props.week
