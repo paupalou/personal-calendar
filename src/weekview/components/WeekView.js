535eeba8918c699ad785e9ad59b475d2375465da
paupalou <paupalou@gmail.com>
2019-02-06 17:23:08 +0100

    Separe sass vars and add css modules (with sass).

--- src/weekview/components/WeekView.js
+++ src/weekview/components/WeekView.js
@@ -1,18 +1,24 @@
 import React from 'react';
 
 import Week from './Week';
+import styles from './WeekView.module.scss';
 
 export default (props) => (
-  <div id="week-view">
+  <div className={styles.container}>
     <Week
       selectedDay={props.selectedDay}
       setSelectedDay={props.setSelectedDay}
     />
 
-    <div id="week-content">
+    <div className={styles.content}>
       <span className="filler" />
       <div></div>
-      <div></div>
+
+      <div className="day-tasks">
+        <div> 12:00 </div>
+        <div> Random Task </div>
+      </div>
+
       <div></div>
       <div></div>
       <div></div>
